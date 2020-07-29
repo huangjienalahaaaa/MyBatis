4b8011aae2fa303059c4cc250802bc030c99d2ac
huangjie <137612270@qq.com>
2020-07-29 11:33:53 +0800

    f

--- project/src/main/java/cn/hj/mapper/UserMapper.java
+++ project/src/main/java/cn/hj/mapper/UserMapper.java
@@ -1,6 +1,5 @@
 package cn.hj.mapper;
 
-import cn.hj.domain.QueryVo;
 import cn.hj.domain.User;
 
 import java.util.List;
@@ -9,19 +8,4 @@ public interface UserMapper {
     public List<User> findAll();
 
     public User findById(Integer userId);
-
-    public void insert(User user);
-
-    public void update(User user);
-
-    public void delete(Integer userId);
-
-    public List<User> findByName(String username);
-
-    public Integer findByCount();
-
-    public List<User> findByVo(QueryVo vo);
-
-    public List<User> findUsers();
-
 }
--- project/src/test/java/cn/hj/test/UserTest.java
+++ project/src/test/java/cn/hj/test/UserTest.java
@@ -34,67 +34,11 @@ public class UserTest {
     }
 
     @Test
-    public void testFindAll() throws Exception {
-        List<User> list = mapper.findAll();
-        for (User user : list) {
-            System.out.println(user);
-        }
-        in.close();
-    }
-
-    @Test
     public void testFindById() throws Exception {
         User user = mapper.findById(41);
         System.out.println(user);
         in.close();
     }
 
-    @Test
-    public void testInsert() throws Exception {
-        User user = new User();
-        user.setUsername("美美");
-        user.setBirthday(new Date());
-        user.setSex("男");
-        user.setAddress("顺义");
-        mapper.insert(user);
-        session.commit();
-        System.out.println(user.getId());
-    }
-
-    @Test
-    public void testUpdate() throws Exception {
-        User user = mapper.findById(41);
-        user.setUsername("小凤");
-        mapper.update(user);
-        session.commit();
-    }
-
-    @Test
-    public void testDelete() throws Exception {
-        mapper.delete(48);
-        session.commit();
-    }
-
-    @Test
-    public void testFindByName() throws Exception {
-        List<User> list = mapper.findByName("%王%");
-        for (User user : list) {
-            System.out.println(user);
-        }
-    }
-
-    @Test
-    public void testFindByName() throws Exception {
-        List<User> list = mapper.findByName("王");
-        for (User user : list) {
-            System.out.println(user);
-        }
-    }
-
-    @Test
-    public void testFindByCount() throws Exception {
-        Integer count = mapper.findByCount();
-        System.out.println("总记录数："+count);
-    }
 
 }
