d0155193080d9b1b267a5abf84dbb30c6afc31c7
huangjie <137612270@qq.com>
2020-07-31 10:52:08 +0800

    f

--- project/src/main/java/cn/hj/mapper/AccountMapper.java
+++ project/src/main/java/cn/hj/mapper/AccountMapper.java
@@ -1,11 +1,29 @@
 package cn.hj.mapper;
 
 import cn.hj.domain.Account;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 
 public interface AccountMapper {
+    /**
+     * 立即加载
+     * @return
+     */
+    @Select("SELECT a.ID AS aid,a.UID,a.MONEY,u.* FROM account a,USER u WHERE a.UID = u.id")
+    @Results(value= {
+            @Result(id=true,column="aid",property="id"),
+            @Result(column="uid",property="uid"),
+            @Result(column="money",property="money"),
+            @Result(column="id",property="user.id"),
+            @Result(column="username",property="user.username"),
+            @Result(column="birthday",property="user.birthday"),
+            @Result(column="sex",property="user.sex"),
+            @Result(column="address",property="user.address")
+    })
     public List<Account> findAll();
 
     public List<Account> findAllLazy();
--- project/src/main/java/cn/hj/mapper/UserMapper.java
+++ project/src/main/java/cn/hj/mapper/UserMapper.java
@@ -12,13 +12,7 @@ public interface UserMapper {
 
 
     @Select("select * from user")
-    /**
-     * @Results注解(作用：封装数据)：
-     *      1. id:给这个注解起名字
-     *      2. @Result():每一个属性要用一个  @Result()
-     */
     @Results(id="userMap",value= {
-//            @Result(column="id",property="id"),
             @Result(id=true,column="id",property="id"),
             @Result(column="username",property="username"),
             @Result(column="birthday",property="birthday"),
@@ -29,30 +23,21 @@ public interface UserMapper {
 
 
     @Select(value="select * from user where id = #{uid}")
-    /**
-     * @ResultMap注解：
-     *         用来引入已经写好的数据封装，这里也就是上面写好的@Results()
-     */
     @ResultMap(value="userMap")
     public User findById(Integer uid);
 
-    // 保存
     @Insert(value="insert into user (username,birthday,sex,address) values (#{username},#{birthday},#{sex},#{address})")
     public void insert(User user);
 
-    // 修改
     @Update("update user set username = #{username},birthday=#{birthday},sex=#{sex},address=#{address} where id = #{id}")
     public void update(User user);
 
-    // 删除
     @Delete("delete from user where id = #{id}")
     public void delete(Integer userId);
 
-    // 查询数量
     @Select("select count(*) from user")
     public Integer findByCount();
 
-    // 模糊查询
     @Select("select * from user where username like #{username}")
     public List<User> findByUsername(String username);
 
