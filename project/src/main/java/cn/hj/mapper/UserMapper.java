86553ededf6a305ce89509a0a608c14406ccebbf
huangjie <137612270@qq.com>
2020-07-31 10:43:41 +0800

    f

--- project/src/main/java/cn/hj/mapper/UserMapper.java
+++ project/src/main/java/cn/hj/mapper/UserMapper.java
@@ -1,7 +1,7 @@
 package cn.hj.mapper;
 
 import cn.hj.domain.User;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -10,14 +10,50 @@ public interface UserMapper {
 
     public List<User> findAllLazy();
 
-    /*查询所有的用户:
-    *
-    *   1. @Select:
-    *           1. @Select(value = {"",""})  多个值的写法
-    *           2. @Select(value = "")  只有一个值的写法
-    *
-    *       注意，values属性是可以省略不写的：既"value =" 是可以不写的
-    * */
+
     @Select("select * from user")
+    /**
+     * @Results注解(作用：封装数据)：
+     *      1. id:给这个注解起名字
+     *      2. @Result():每一个属性要用一个  @Result()
+     */
+    @Results(id="userMap",value= {
+//            @Result(column="id",property="id"),
+            @Result(id=true,column="id",property="id"),
+            @Result(column="username",property="username"),
+            @Result(column="birthday",property="birthday"),
+            @Result(column="sex",property="sex"),
+            @Result(column="address",property="address")
+    })
     public List<User> findAll();
+
+
+    @Select(value="select * from user where id = #{uid}")
+    /**
+     * @ResultMap注解：
+     *         用来引入已经写好的数据封装，这里也就是上面写好的@Results()
+     */
+    @ResultMap(value="userMap")
+    public User findById(Integer uid);
+
+    // 保存
+    @Insert(value="insert into user (username,birthday,sex,address) values (#{username},#{birthday},#{sex},#{address})")
+    public void insert(User user);
+
+    // 修改
+    @Update("update user set username = #{username},birthday=#{birthday},sex=#{sex},address=#{address} where id = #{id}")
+    public void update(User user);
+
+    // 删除
+    @Delete("delete from user where id = #{id}")
+    public void delete(Integer userId);
+
+    // 查询数量
+    @Select("select count(*) from user")
+    public Integer findByCount();
+
+    // 模糊查询
+    @Select("select * from user where username like #{username}")
+    public List<User> findByUsername(String username);
+
 }
--- project/src/test/java/cn/hj/test/Test.java
+++ project/src/test/java/cn/hj/test/Test.java
@@ -21,31 +21,112 @@ public class UserTest {
     private SqlSession session;
     private UserMapper mapper;
 
+    // 初始化的方法
     @Before
     public void init() throws Exception {
+        // 加载主配置文件
         in = Resources.getResourceAsStream("SqlMapConfig.xml");
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        // 创建工厂对象
+        factory = new SqlSessionFactoryBuilder().build(in);
+        // 创建session
         session = factory.openSession();
+        // 获取到代理对象
         mapper = session.getMapper(UserMapper.class);
     }
 
+    // 销毁的方法
     @After
-    public void destory() throws IOException {
-        in.close();
+    public void destory() throws Exception {
+        // 关闭资源
         session.close();
+        in.close();
     }
 
-
-    //测试 注解的入门程序
+    /**
+     * 测试查询
+     * @throws Exception
+     */
     @Test
-    public void testFindByForeach() throws Exception {
-
-        List<User> list= mapper.findAll();
-       for(User user :list){
-           System.out.println(user);
-       }
-       session.close();
-       InputStream.close();
-
+    public void testFindAll() throws Exception {
+        // 调用方法
+        List<User> list = mapper.findAll();
+        for (User user : list) {
+            System.out.println(user);
+        }
     }
+
+    @Test
+    public void testFindById() {
+        User user = mapper.findById(41);
+        System.out.println(user);
+    }
+
+    /**
+     * 保存
+     * @throws Exception
+     */
+    @Test
+    public void testInsert() throws Exception {
+        // 创建User对象
+        User user = new User();
+        user.setUsername("美美2");
+        user.setBirthday(new Date());
+        user.setSex("女");
+        user.setAddress("监狱");
+
+        // 保存
+        mapper.insert(user);
+        session.commit();
+    }
+
+    /**
+     * 测试
+     * @throws Exception
+     */
+    @Test
+    public void testUpdate() throws Exception {
+        // 先通过id查询
+        User user = mapper.findById(41);
+        // 设置新的数据内容
+        user.setUsername("熊大");
+        user.setAddress("深林深处");
+
+        // 修改
+        mapper.update(user);
+        session.commit();
+    }
+
+    /**
+     * 删除
+     * @throws Exception
+     */
+    @Test
+    public void testDelete() throws Exception {
+        mapper.delete(42);
+        session.commit();
+    }
+
+    /**
+     * 查询的是聚合函数
+     * @throws Exception
+     */
+    @Test
+    public void testFindByCount() throws Exception {
+        Integer count = mapper.findByCount();
+        System.out.println("总数量："+count);
+    }
+
+    /**
+     * 模糊查询
+     * @throws Exception
+     */
+    @Test
+    public void testFindByUsername() throws Exception {
+        // 第一种测试的方式
+        List<User> list = mapper.findByUsername("%王%");
+        for (User user : list) {
+            System.out.println(user);
+        }
+    }
+
 }
